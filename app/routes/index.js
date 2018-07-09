b047f06698870254a9e62b026e7b8c77b5273cdc
vinay.katdare <vinay.katdare@gmail.com>
2018-07-09 13:25:08 +0530

    Changes in Login Authentication

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,37 @@ var passport 	= require('passport');
 var User = require('../models/user');
 var Room = require('../models/room');
 
+var axios = require('axios');
+
+// export const productionAddr = 'http://10.2.0.100:8000'
+const productionAddr = 'http://localhost:8082'
+
+const serverAddr = axios.create({
+    baseURL: productionAddr,
+    timeout: 30000,
+    headers: {},
+    withCredentials: true,
+    transformRequest: [function (data, headers) {
+      // Do whatever you want to transform the data
+    //   const access_token = localStorage.getItem("x-access-token");
+    //   const refresh_token = localStorage.getItem("x-refresh-token");
+    //   if(access_token && refresh_token){
+    //     headers['x-access-token'] = access_token;
+    //     headers['x-refresh-token'] = refresh_token;
+    //   }
+      headers['Content-Type'] = "application/json;charset=UTF-8";
+      return JSON.stringify(data);
+    }],
+    transformResponse: [function(data) {
+        data = JSON.parse(data)
+        if ("tokenParams" in data && data.tokenParams.isTokenRefreshed){
+			delete serverAddr.defaults.headers['x-access-token'];
+			serverAddr.defaults.headers.post['x-access-token'] = data.tokenParams["x-access-token"];
+        }
+        return data;
+    }]
+})
+
 // Home page
 router.get('/', function(req, res, next) {
 	// If user is already logged in, then redirect to rooms page
@@ -16,18 +47,39 @@ router.get('/', function(req, res, next) {
 	else{
 		res.render('login', {
 			success: req.flash('success')[0],
-			errors: req.flash('error'), 
+			errors: req.flash('error'),
 			showRegisterForm: req.flash('showRegisterForm')[0]
 		});
 	}
 });
 
 // Login
-router.post('/login', passport.authenticate('local', { 
-	successRedirect: '/rooms', 
-	failureRedirect: '/',
-	failureFlash: true
-}));
+// router.post('/login', passport.authenticate('local', {
+// 	successRedirect: '/rooms',
+// 	failureRedirect: '/',
+// 	failureFlash: true
+// }));
+router.post('/login', function(req, res, next){
+	serverAddr.post('/v1/user-login/',{
+		"username" : req.body.username,
+		"password" : req.body.password
+	})
+	.then(function(data){
+		console.log("User Login: ", data.data);
+		if(data.data.status && data.data.data.length>0){
+			serverAddr.defaults.headers.post['x-access-token'] = data.data["x-access-token"];
+			serverAddr.defaults.headers.post['x-refresh-token'] = data.data["x-refresh-token"];
+			res.redirect('/rooms');
+		} else {
+			req.flash('error', 'Invalid credentials');
+			res.redirect('/');
+		}
+	})
+	.catch(function(error){
+ 		console.log(error);
+	})
+});
+
 
 // Register via username and password
 router.post('/register', function(req, res, next) {
@@ -83,17 +135,17 @@ router.get('/rooms', [User.isAuthenticated, function(req, res, next) {
 	});
 }]);
 
-// Chat Room 
+// Chat Room
 router.get('/chat/:id', [User.isAuthenticated, function(req, res, next) {
 	var roomId = req.params.id;
 	Room.findById(roomId, function(err, room){
 		if(err) throw err;
 		if(!room){
-			return next(); 
+			return next();
 		}
 		res.render('chatroom', { user: req.user, room: room });
 	});
-	
+
 }]);
 
 // Logout
